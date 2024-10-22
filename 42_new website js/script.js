1945588e964e61efa52e08d2632dcf4039b41f19
santhoshzoro <santhoshreddy.ganapathi@gmail.com>
2024-10-22 15:55:50 +0530

    new-repository

---
+++ 42_new website js/script.js
@@ -0,0 +1,65 @@
+var flipkart=[
+    {
+
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/698ba0cebe456aaf.jpg?q=100'
+        
+    },
+    {
+
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/4da1d0d19350cc84.jpg?q=100'
+    },
+    {
+
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/9d4e9c605fc1d2d3.jpg?q=100'
+    },
+    {
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/a5e656672d0548dd.jpg?q=100'
+    },
+    {
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/5b813b64a3179898.jpg?q=100'
+    },
+    {
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/7a5e96c10ada8a56.jpg?q=100'
+    },
+    {
+
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/57fe1ffe54569c41.jpg?q=100'
+    },
+    {
+
+        imgsrc:'https://rukminim2.flixcart.com/fk-p-flap/80/80/image/25f400c36bc3487d.jpg?q=100'
+    },
+
+];
+
+document.getElementById('a').src=flipkart[0].imgsrc
+var topOffers1='Top offers'
+document.getElementById('top offers').innerHTML=topOffers1
+// ..............................................................
+document.getElementById('b').src=flipkart[1].imgsrc
+var Electronic1='Electronics'
+document.getElementById('Electronic').innerHTML=Electronic1
+// ....................................................................
+document.getElementById('c').src=flipkart[2].imgsrc
+var Fasion1='Fasion'
+document.getElementById('Fasion').innerHTML=Fasion1
+// ................................................................
+document.getElementById('d').src=flipkart[3].imgsrc
+var Beauty1='Beauty'
+document.getElementById('Beauty').innerHTML=Beauty1
+// .....................................................................
+document.getElementById('e').src=flipkart[4].imgsrc
+var HomeKitchen1='Home&Kitchen'
+document.getElementById('Home&kitchen').innerHTML=HomeKitchen1
+// ......................................................................
+document.getElementById('f').src=flipkart[5].imgsrc
+var Furniture1='Furniture'
+document.getElementById('Furniture').innerHTML=Furniture1
+// ................................................................
+document.getElementById('g').src=flipkart[6].imgsrc
+var Travel1='Travel'
+document.getElementById('Travel').innerHTML=Travel1
+// .................................................................
+document.getElementById('h').src=flipkart[7].imgsrc
+var Groceries1='Groceries'
+document.getElementById('Groceries').innerHTML=Groceries1
